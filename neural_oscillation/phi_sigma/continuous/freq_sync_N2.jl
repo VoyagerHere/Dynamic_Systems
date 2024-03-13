08ee6451f0e3ef5875267ea919044b2af9bd9829
VoyagerHere <kolessnikoff.d.e@gmail.com>
2024-03-13 23:51:39 +0300

    Add plotters phi_sigma

---
+++ neural_oscillation/phi_sigma/continuous/freq_sync_N2.jl
@@ -0,0 +1,231 @@
+using DifferentialEquations
+using Plots
+using LaTeXStrings
+using JLD
+using Statistics
+using Dates
+
+
+const k_DEBUG_PRINT = false
+const k_DRAW_PHASE_REALISATION = false;
+const k_IS_SAVE_DATA = true;
+const k_DELETE_TRANSIENT = false;
+const k_DELETE_UNSTABLE = false;
+const k_PRINT_ITERATION = false;
+
+const DATA_TAKE_ERROR = 0.25;
+
+name = "pi_8__2_2"
+const ALPHA = pi / 8
+N1 = 2
+N2 = 2
+
+const NUM = 2;
+global PAR_N = [N1, N2];
+const D_MAX =  0.03
+const D_ACCURACY =  0.00001
+const SYNC_ERROR =  0.05
+const GStart =  1.01
+const DELTA = 0.005;
+D_LIST = 0:D_ACCURACY:D_MAX
+D_NUM = length(D_LIST)
+
+G1 = 1.01;
+G2 = G1 + DELTA;
+
+const NUM_OF_COMPUTE_RES = 3;
+DATA = [zeros(NUM_OF_COMPUTE_RES) for _ in 1:D_NUM]
+W = [zeros(NUM*2) for _ in 1:D_NUM]
+function eqn!(du, u, p, t)
+  d, alpha, g, n, dim_size = p
+  f = g .- sin.(u ./ n)
+  exch = zeros(dim_size)
+  for i in 1:dim_size
+    for j in 1:dim_size-1
+      exch[i] += d[j] * sin(u[j] - u[i] - alpha)
+    end
+  end
+  du .= f + exch
+end
+
+function FREQ_SYNC(DATA, G1, G2, PAR_N, NUM, D_LIST, ALPHA)
+  num_of_iterations = length(D_LIST)
+  a = 8000;
+  b = 10000;
+
+  for m in eachindex(D_LIST)    
+    tspan = (a, b)
+    global D = D_LIST[m]
+    # global D = 0.0255
+    
+    p = (D, ALPHA, [G1, G2], PAR_N, NUM);
+    y0 = [0; 0]
+
+    prob = ODEProblem(eqn!, y0, tspan, p)
+    sol = solve(prob, Tsit5(), reltol=1e-12, abstol=1e-12)
+    global Y = sol.u;
+    global T = sol.t;
+
+    if (k_DELETE_TRANSIENT)
+      index = DELETE_TRANSIENT(Y)
+      start = T[index];
+      y0 = [start, start]
+
+      prob = ODEProblem(eqn!, y0, tspan, p)
+      sol = solve(prob, Tsit5(), reltol=1e-12, abstol=1e-12)
+      Y = sol.u;
+      T = sol.t;
+    end
+
+    w_s, w_b, err = SYNC_PAIR(T, Y, PAR_N)
+
+    ratio_s = w_s[1] / w_s[2];
+    ratio_b = w_b[1] / w_b[2];
+
+    if (err != 0)
+      DATA[m] = [D, -err, -err]
+      continue;
+    end
+
+    DATA[m] = [D, ratio_s, ratio_b]
+    W[m] = vcat(w_s, w_b)
+    println("Iteration $m of $num_of_iterations")
+    # return
+  end
+end
+
+function SYNC_PAIR(T, Y, PAR_N)
+  Y = reduce(vcat,transpose.(Y))
+  global SPIKES1, err1 = FIND_SPIKES(Y[:,1], PAR_N[1])
+  global SPIKES2, err2 = FIND_SPIKES(Y[:,2], PAR_N[2])
+
+  k_DEBUG_PRINT && println("Spikes in 1: ", length(SPIKES1))
+  k_DEBUG_PRINT && println("Spikes in 2: ", length(SPIKES2))
+
+  err = handle_errors(Bool(err1), Bool(err2));
+  if err != 0
+      return ([0, 0], [0, 0], err)
+  end
+
+  Times_1 = FIND_TIMES(SPIKES1, T, PAR_N);
+  Times_2 = FIND_TIMES(SPIKES2, T, PAR_N);
+
+  Times_SP_1 = DEL_MIDDLE_BURST_INTRVL(PAR_N[1], Times_1)
+  Times_SP_2 = DEL_MIDDLE_BURST_INTRVL(PAR_N[2], Times_2)
+
+  avg(x) = (ones(length(x)) / length(x))'*x
+
+  wb_1 = 2*pi./sum(Times_1);
+  wb_2 = 2*pi./sum(Times_2);
+
+  global ws_1 = 2*pi./avg(Times_SP_1);
+  global ws_2 = 2*pi./avg(Times_SP_2);
+
+  return ([ws_1, ws_2], [wb_1, wb_2], err)
+end
+
+avg(x) = (ones(length(x)) / length(x))'*x
+
+function FIND_SPIKES(Y, n)
+  Y = mod.(Y, 2*pi)
+  SPIKES = findall(x -> abs.(x - 2*pi) < DATA_TAKE_ERROR, Y )
+  FIND_NEAR_POINTS(SPIKES)
+  if (length(SPIKES)/n < 3)
+    err = 1;
+  else
+    err = 0;
+  end
+  return (SPIKES, err)
+end
+
+
+function FIND_TIMES(SPIKES, T, NUM)
+  len = maximum(NUM);
+  TIMES = zeros(len);
+  for k in 2:(len+1)
+    val1 = SPIKES[k-1]
+    val2 = SPIKES[k]
+    TIMES[k-1] = T[val2] - T[val1];
+  end
+  return TIMES;
+end
+
+function DEL_MIDDLE_BURST_INTRVL(n, T)
+  if (n > 1)
+    T = sort(T, rev = true)
+    T = T[2:end]
+  end
+  return T;
+end
+
+function handle_errors(err1::Bool, err2::Bool)
+  if err1 && err2
+      return  3
+  elseif err2
+      return  2
+  elseif err1
+      return  1
+  else
+      return  0
+  end
+end
+
+
+function DELETE_TRANSIENT(Y, tol=0.002)
+  len = length(Y);
+  i = 10;
+  while i < len
+      rel_change_1 = abs((Y[i][1] - Y[i-1][1]) / Y[i-1][1])
+      rel_change_2 = abs((Y[i][2] - Y[i-1][2]) / Y[i-1][2])
+      if ((rel_change_1 < tol) && (rel_change_2 < tol))
+          return i
+      end
+      i += 10;
+  end
+  return 1
+end
+
+
+function FIND_NEAR_POINTS(POINTS)
+  i = 1;
+    while i < length(POINTS)
+        if POINTS[i+1] - POINTS[i] == 1
+            deleteat!(POINTS, i)
+        else
+            i +=  1
+        end
+    end
+end
+
+function IS_SYNC(DIFF, SYNC_ERROR)
+  if (DIFF_SPIKES(DIFF, SYNC_ERROR))
+    return 1;
+  end
+  return 0;
+end
+
+function DIFF_SPIKES(DIFF, SYNC_ERROR)
+  mn = mean(abs.(DIFF))
+  return all(abs.(abs.(DIFF) .- mn) .< SYNC_ERROR)
+end
+
+function DRAW(T, Y, G1, G2, D, PAR_N)
+  Y = [mod.(y, 2 * pi) for y in Y]
+  n1 = PAR_N[1];
+  n2 = PAR_N[2];
+  plot(T, getindex.(Y, 1), label=L"n_1 = %$n1, \gamma_{1}=%$G1")
+  plot!(T, getindex.(Y, 2), label=L"n_2 = %$n2 , \gamma_{2}=%$G2")
+  title!(L"d = %$D")
+  ylims!(0,  2*pi)
+  xlabel!(L"t")
+  ylabel!(L"\varphi")
+end
+
+FREQ_SYNC(DATA, G1, G2, PAR_N, NUM, D_LIST, ALPHA);
+
+
+if k_IS_SAVE_DATA 
+  times = Dates.format(now(),"__yyyymmdd_HHMM");
+  filename ="$name$times.jld2"
+  @save filename DATA W
+end
---
+++ neural_oscillation/phi_sigma/continuous/freq_sync_plotter_N2_W_B.jl
@@ -0,0 +1,60 @@
+using DifferentialEquations
+using LaTeXStrings
+using JLD
+using Statistics
+using Dates
+using Plots
+gr()
+
+alpha_txt = "π/8"
+N1 = 3;
+N2 = 3;
+@load "pi_8__3_3__20240313_1818.jld2" DATA
+
+size = length(DATA)
+DATA = reduce(vcat,transpose.(DATA))
+D_VEC = DATA[:,1]
+# RATIO_S = DATA[:,2]
+RATIO_B = DATA[:,3]
+
+# RATIO_B = round.(RATIO_B; digits = 3);
+
+GOOD =  findall(x -> x > 0, RATIO_B[:,1])
+DEAD1 = findall(x -> x ==  -1.0, RATIO_B[:,1])
+DEAD2 = findall(x -> x ==  -2.0, RATIO_B[:,1])
+DEAD3 = findall(x -> x ==  -3.0, RATIO_B[:,1])
+
+
+rectangle(w, h, x, y) = Shape(x .+ [0,w,w,0], y .+ [0,0,h,h])
+
+
+if length(GOOD) > 0
+  plot(D_VEC[GOOD], RATIO_B[GOOD], label=L"\frac{w_{b}^{1}}{w_{b}^{2}}(d)")
+  scatter!([minimum(D_VEC)],[0], label=" ", ms=0, mc=:white, msc=:white)
+end
+if length(DEAD1) > 0
+  plot!(rectangle(maximum(D_VEC[DEAD1])-minimum(D_VEC[DEAD1]),1,minimum(D_VEC[DEAD1]),0), opacity=.5, color = colorant"grey44", label = L"Death $\; \varphi_1$")
+  scatter!([minimum(D_VEC)],[0], label=" ", ms=0, mc=:white, msc=:white)
+end
+if length(DEAD2) > 0
+  plot!(rectangle(maximum(D_VEC[DEAD2])-minimum(D_VEC[DEAD2]),1,minimum(D_VEC[DEAD2]),0), opacity=.5, color = colorant"grey34", label = L"Death $\varphi_2$")
+  scatter!([minimum(D_VEC)],[0], label=" ", ms=0, mc=:white, msc=:white)
+end
+if length(DEAD3) > 0
+  plot!(rectangle(maximum(D_VEC[DEAD3])-minimum(D_VEC[DEAD3]),1,minimum(D_VEC[DEAD3]),0), opacity=.5, color = colorant"black",  label = L"Death $\varphi_1, \varphi_2$")
+end
+
+# # Calculate center points for each area
+# center_DEAD1 = (minimum(D_VEC[DEAD1]) + maximum(D_VEC[DEAD1])) /  2
+# center_DEAD2 = (minimum(D_VEC[DEAD2]) + maximum(D_VEC[DEAD2])) /  2
+# center_DEAD3 = (minimum(D_VEC[DEAD3]) + maximum(D_VEC[DEAD3])) /  2
+
+# annotate!(center_DEAD1,  0.5, text("DEAD1", :center,  5))
+# annotate!(center_DEAD3,  0.5, text("Death3", :center,  5))
+
+title!(L"$n_1$ = %$N1, $n_2$ = %$N2, $\alpha$ = %$alpha_txt")
+ylims!(0,  1)
+xlabel!(L"d", guidefontsize=16)
+ylabel!(L"w_{b}^{1}/w_{b}^{2}", guidefontsize=16)
+plot!(legendfontsize=10, legend=:outertopright) 
+savefig("freq_plot_b.png")
