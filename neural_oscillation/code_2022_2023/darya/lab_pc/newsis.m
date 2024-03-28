15d360039db68a23c24ac16f0640444a18e249e8
VoyagerHere <kolessnikoff.d.e@gmail.com>
2024-03-28 16:10:27 +0300

    update

--- neural_oscillation/code_2022_2023/darya/lab_pc/newsis.m
+++ neural_oscillation/code_2022_2023/darya/lab_pc/newsis.m
@@ -5,10 +5,10 @@ n1 = 1;
 n2 = 1;
 k=-500;
 
-y0 = [(pi/2); (pi/2)]; %временные рамки для построения
+y0 = [0; 0]; %временные рамки для построения
 %y0 = [pi; pi];
-a=2000;
-b=3000;
+a=100;
+b=300;
 
 %I1=1/(1+exp^(k*(cos(sigma)-sin(y.))));
 %I2=1/(1+exp^(k*(cos(sigma)-sin(y.))));
@@ -18,8 +18,7 @@ opts = odeset('RelTol',2e-13,'AbsTol',1e-100); %решение системы Д
 
 
 %построение графика временной реализации
-close all
-%Y=mod(Y, 2*pi) ;
+Y=mod(Y, 2*pi) ;
 plot(T, Y(:,1),'-b', T, Y(:,2),'-r');
 ylim([0 2*pi]); yticks([0 pi 2*pi]); yticklabels(["0" "\pi" "2\pi"]);
 
@@ -31,8 +30,8 @@ ylabel('\theta', 'Interpreter','tex');
 
 %построение ФП
 %plot(Y(:,1), Y(:,2),'-r');
-%ylim([0 2*pi]); yticks([0 pi 2*pi]); yticklabels(["0" "\pi" "2\pi"]);
-%xlim([0 2*pi]); xticks([0 pi 2*pi]); xticklabels(["0" "\pi" "2\pi"]);
+% ylim([0 2*pi]); yticks([0 pi 2*pi]); yticklabels(["0" "\pi" "2\pi"]);
+% xlim([0 2*pi]); xticks([0 pi 2*pi]); xticklabels(["0" "\pi" "2\pi"]);
 %title('d=0.15', 'sigma = 1');
 %legend('n_1=1','n_2=1', 'Interpreter','tex');
 %xlabel('\theta_1', 'Interpreter','tex');
@@ -43,7 +42,7 @@ ylabel('\theta', 'Interpreter','tex');
 %задание ДУ
 
 function dy_dt = eqn(~,y,k ,sigma ,d,n1,n2)
-g = [1.01; 1.02];
+g = [1.01; 1.01 + 0.000005];
 no = [n1; n2];
 f = g - sin(y./no);
 exch = d*(1./(1+exp(k*(cos(sigma)-sin(y([2;1]))))));
