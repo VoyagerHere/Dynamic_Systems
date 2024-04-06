aa0a687429d63de7cfaf749557c688929ec9c647
VoyagerHere <kolessnikoff.d.e@gmail.com>
2024-04-06 03:17:27 +0300

    fix kuramoto death

--- neural_oscillation/phi_kuramoto/compute/death/pi_2_3__3_3_3.jl
+++ neural_oscillation/phi_kuramoto/compute/death/pi_2_3__3_3_3.jl
@@ -32,14 +32,13 @@ const ALPHA = 2*pi/3
 
 const NUM = 3;
 const PAR_N = [N1, N2, N3];
-const D_MAX =  0.1
-const D_ACCURACY =  0.0001
+const D_ACCURACY =  0.001
 const G_NUM = 640
 const SYNC_ERROR =  0.25
 const GStart =  1.01
 const DELTA =  0.025
 G_LIST = range(GStart, stop=GStart + DELTA, length=G_NUM)
-D_LIST = 0:D_ACCURACY:D_MAX
+D_LIST = 2.5:D_ACCURACY:2.8
 D_NUM = length(D_LIST)
 
 const NUM_OF_COMPUTE_RES = 4;
