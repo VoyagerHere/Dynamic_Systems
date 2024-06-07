4be0528eaa59e9320121f5f69ad22bef41baeaa0
VoyagerHere <kolessnikoff.d.e@gmail.com>
2024-06-07 11:31:33 +0300

    add 2DIM more accuracy

--- neural_oscillation/phi_sigma/discrete/compute/fr_discr_2DIM.sh
+++ neural_oscillation/phi_sigma/discrete/compute/fr_discr_2DIM.sh
@@ -1,13 +1,13 @@
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_sin_2DIM_110.005.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_sin_2DIM_110.01.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_sin_2DIM_110.05.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_sin_2DIM_330.005.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_sin_2DIM_330.01.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_sin_2DIM_330.05.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_sin_2DIM_110.005.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_sin_2DIM_110.01.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_sin_2DIM_110.05.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_sin_2DIM_330.005.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_sin_2DIM_330.01.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_sin_2DIM_330.05.jl
 
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_cos_2DIM_110.005.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_cos_2DIM_110.01.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_cos_2DIM_110.05.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_cos_2DIM_330.005.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_cos_2DIM_330.01.jl
-srun -p a100 -c 32 -t 4320 ~/julia-1.10.2/bin/julia -t 32 fr_discr_cos_2DIM_330.05.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_cos_2DIM_110.005.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_cos_2DIM_110.01.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_cos_2DIM_110.05.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_cos_2DIM_330.005.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_cos_2DIM_330.01.jl
+srun -p a100 -c 64 -t 4320 ~/julia-1.10.2/bin/julia -t 64 fr_discr_cos_2DIM_330.05.jl
